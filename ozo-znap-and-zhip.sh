fe72faa4f73b4f992a8fc4307b9dc541aeb4b95a
Andy Lievertz <andy.lievertz@gmail.com>
2023-03-07 00:15:13 -0700

    basic json and venv requirements

--- ozo-znap-and-zhip.sh
+++ ozo-znap-and-zhip.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+#source
+ssh root@trei.zenastronave.com zfs create -s -o compression=on -o dedup=off -o volblocksize=64k -V 60GB trei-pool/plex
+
+ssh -o BatchMode=yes root@trei.zenastronave.com true
 ssh root@trei.zenastronave.com zfs snapshot trei-pool/plex@trei-20230307-032100
+
+# initial znap n zhip (-p)
+ssh root@trei.zenastronave.com zfs send -p trei-pool/plex@trei-20230307-032100 | zfs recv -ev mini-pool/trei
+
+# subsequent (incremental) znap n zhips (-p -i)
 ssh root@trei.zenastronave.com zfs send -p trei-pool/plex@trei-20230307-032100 | zfs recv -ev mini-pool/trei
